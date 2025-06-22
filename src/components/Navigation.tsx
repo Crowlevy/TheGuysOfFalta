4ab3c3acc119564139a5631117fe920b1bc6e7f9
Crowlevy <slootazzykingz@gmail.com>
2025-06-22 18:33:37 -0300

    fix: import icons in notification

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,7 @@
+import { HomeIcon, ClockIcon, TrophyIcon, Cog  } from "lucide-react";
 const navigation = [
   { name: 'Dashboard', href: '/dashboard', icon: HomeIcon },
   { name: 'Histórico', href: '/history', icon: ClockIcon },
   { name: 'Conquistas', href: '/badges', icon: TrophyIcon },
-  { name: 'Configurações', href: '/settings', icon: Cog6ToothIcon },
+  { name: 'Configurações', href: '/settings', icon: Cog },
 ]; 
